668f68235b1f58beb4dce3afa24d51f17597d3b0
VPoint <estar.raji@gmail.com>
2017-07-09 21:39:50 -0400

    Ground Pick Up
    Ground sequence works. Updates Safe. And implemented Sink. Changed
    Collider for Bed Zoom.

--- Assets/Scripts/QuestTrigger.cs
+++ Assets/Scripts/QuestTrigger.cs
@@ -27,17 +27,16 @@ public class QuestTrigger : MonoBehaviour
 
     void OnMouseDown()
     {
-        Debug.Log("Quest Trigger Mouse down method");
+        //Debug.Log("Quest Trigger Mouse down method");
 		//Debug.Log(startQuest.ToString() + " and " + theQM.quests[questNumber].gameObject.activeSelf.ToString());
         if (!theQM.questCompleted[questNumber])
         {
             //Debug.Log("Mouse down method activated");
-			Debug.Log("Quest Nto Completed");
             if (startQuest && !theQM.quests[questNumber].gameObject.activeSelf)
             {
-                Debug.Log("Show the Text already");
 				theQM.quests[questNumber].gameObject.SetActive(true);
                 theQM.quests[questNumber].StartQuest();
+				Destroy(gameObject);
             }
 
             if (endQuest && theQM.quests[questNumber].gameObject.activeSelf)
