8b224aed269c202eb0e127d95f3816b4adacbe6d
VPoint <estar.raji@gmail.com>
2017-07-17 18:39:43 -0400

    Add images and reimplemented persistent buttons

--- Assets/Scripts/Action.cs
+++ Assets/Scripts/Action.cs
@@ -1,29 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Action : MonoBehaviour
+public class Action : Button
 {
 
     private GameModel theGM;
+	private GameModel.State buttonState;
     public int instructionID;
+	public Image buttonImage;
+	public Sprite selected;
+	public Sprite deselected;
 
     // Use this for initialization
     void Start()
     {
         theGM = FindObjectOfType<GameModel>();
+		buttonState = (GameModel.State)(instructionID);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
+		if(theGM.currState == buttonState){
+			//Debug.Log("Show pressed");
+			buttonImage.sprite = selected;
+		} else {
+			buttonImage.sprite = deselected;
+		}
     }
 
-    public void ChangeState()
+    public void changeState()
     {
-        Debug.Log("Action Selected");
-        theGM.SetState(instructionID);
+        //Debug.Log("Action Selected");
+		if(theGM.currState != buttonState){
+			theGM.SetState(instructionID);
+		} else {
+			theGM.SetState(0);
+		}
     }
 }
